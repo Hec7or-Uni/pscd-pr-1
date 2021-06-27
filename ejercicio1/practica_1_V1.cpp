50d3b4a44280c5482201217884c5f87434f55331
Hector Toral <hectortoraltrabajos@gmail.com>
2021-06-27 23:48:34 +0200

    Update practica_1_V1.cpp

--- ejercicio1/practica_1_V1.cpp
+++ ejercicio1/practica_1_V1.cpp
@@ -1,16 +1,7 @@
-//------------------------------------------------------------------------------
-// File:   practica_1_V1.cpp
-// Author: PSCD-Unizar
-// Date:   septiembre 2020
-// Coms:   Parte del práctica 1 de PSCD
-//         Compilar mediante
-//           g++ practica_1_V1.cpp -o practica_1_V1 -std=c++11 -pthread
-//------------------------------------------------------------------------------
-
-#include <iostream>
-#include <thread>
-#include <string>
-#include <chrono>
+# include <iostream>
+# include <thread>
+# include <string>
+# include <chrono>
 
 using namespace std;
 
