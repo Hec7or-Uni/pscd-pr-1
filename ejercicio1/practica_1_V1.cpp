036ab0e6280c9dd610171f4d6fe885f40c2e08c4
Hector Toral <hectortoraltrabajos@gmail.com>
2021-06-27 23:50:08 +0200

    Update practica_1_V1.cpp

--- ejercicio1/practica_1_V1.cpp
+++ ejercicio1/practica_1_V1.cpp
@@ -8,18 +8,16 @@ using namespace std;
 void saludo(string nombre, int retardo, int veces) {
     for(int i=1; i<=veces; i++) {
         cout << "Soy " << nombre << endl;
-        //cout << "Soy " + nombre + "\n";
-        //el thread que me ejecuta se bloquea durante "retardo" milisegundos
         this_thread::sleep_for(chrono::milliseconds(retardo));
     }
 }
 
 int main(int argc, char* argv[]) {
-    thread th_1(&saludo, "Aurora", 100, 10);   //th_1 se pone en marcha
+    thread th_1(&saludo, "Aurora", 100, 10);
     thread th_2(&saludo, "Baltasar", 150, 15);
     thread th_3(&saludo, "Carmen", 300, 5);
 
-    th_1.join(); //me bloqueo hasta que "th_1" termine
+    th_1.join();
     th_2.join();
     th_3.join();
 
